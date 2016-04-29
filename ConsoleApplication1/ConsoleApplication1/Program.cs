b28aac4b815398c4fdf109caa19711befc9d76d4
HoussemEddineShili <webmaster0909@gmail.com>
2016-04-29 09:11:26 +0100

    Log4Net & T4 Tutorial
    Log4Net & T4 Tutorial

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+       (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.GlobalContext.Properties["tab"] = "\t";
+            ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            log.Debug("This is a debug message");
+            log.Warn("This is a warn message");
+            log.Error("This is a error message");
+            log.Fatal("This is a fatal message");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/TextTemplate1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleApplication1.Classes;
+
+namespace ConsoleApplication1
+{
+	public class ClassT4
+	{
+	
+	public Class1 class1  {get; set; }
+	
+	public Class2 class2  {get; set; }
+	
+	}
+}
+
